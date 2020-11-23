079e1c0bc2ee1ac8d5d53f64d727efe7b6f57e53
Jason J. W <71363607+digaji@users.noreply.github.com>
2020-11-24 00:01:05 +0700

    Change to toString, replace lambda, added unittest

--- Class Assignment/Exercise_class_composition.py
+++ Class Assignment/Exercise_class_composition.py
@@ -18,7 +18,7 @@ class Author:
     def getGender(self):
         return self.__gender
 
-    def __str__(self):
+    def toString(self):
         return f"Author[name={self.__name},email={self.__email},gender={self.__gender}]"
 
 
@@ -47,8 +47,8 @@ class Book:
     def setQty(self, qty):
         self.__qty = qty
 
-    def __str__(self):
-        return f"Book[name={self.__name},{self.__author},price={self.__price},qty={self.__qty}]"
+    def toString(self):
+        return f"Book[name={self.__name},{self.__author.toString()},price={self.__price},qty={self.__qty}]"
 
     def getAuthorName(self):
         return self.__author.getName()
@@ -60,12 +60,12 @@ class Book:
         return self.__author.getGender()
 
 
-radit = Author("Radit", "radit@radit.com", "m")
-cinta = Book("Cinta Brontosaurus", radit, 100, 10)
+# radit = Author("Radit", "radit@radit.com", "m")
+# cinta = Book("Cinta Brontosaurus", radit, 100, 10)
 
-print(cinta)
-print(cinta.getAuthor().getName(), cinta.getAuthor().getEmail())
-print(cinta.getAuthorName(), cinta.getAuthorEmail(), cinta.getAuthorGender())
+# print(cinta.toString())
+# print(cinta.getAuthor().getName(), cinta.getAuthor().getEmail())
+# print(cinta.getAuthorName(), cinta.getAuthorEmail(), cinta.getAuthorGender())
 
 
 class Book:
@@ -93,26 +93,67 @@ class Book:
     def setQty(self, qty):
         self.__qty = qty
 
-    def __str__(self):
+    def toString(self):
         return (
             f"Book[name={self.__name},"
-            f"authors="
-            + "{"
-            + str(list(map(lambda x: str(x), self.__authors))).replace("'", "").strip("[]")
+            "authors="
+            "{"
+            + str(list(x.toString() for x in self.__authors)).replace("'", "").strip("[]")
             + "]},"
-            + f"price={self.__price},"
+            f"price={self.__price},"
             f"qty={self.__qty}]"
         )
 
     def getAuthorNames(self):
         return (
-            str(list(map(lambda x: x.getName(), self.__authors))).replace("'", '"').strip("[]")
+            str(list(x.getName() for x in self.__authors)).replace("'", "").replace(" ", "").strip("[]")
         )
 
 
-radit = Author("Radit", "radit@radit.com", "m")
-boyke = Author("Boyke", "boyke@boyke.com", "m")
-cinta = Book("Cinta Brontosaurus", [radit, boyke], 100, 10)
+# radit = Author("Radit", "radit@radit.com", "m")
+# boyke = Author("Boyke", "boyke@boyke.com", "m")
+# cinta = Book("Cinta Brontosaurus", [radit, boyke], 100, 10)
 
-print(cinta)
-print(cinta.getAuthorNames())
+# print(cinta.toString())
+# print(cinta.getAuthorNames())
+
+import unittest
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.radit = Author("Radit", "radit@radit.com", "m")
+        self.boyke = Author("Boyke", "boyke@boyke.com", "m")
+        self.cinta = Book("Cinta Brontosaurus", [self.radit, self.boyke], 100, 10)
+
+    def test_getName(self):
+        self.assertEqual(self.cinta.getName(), "Cinta Brontosaurus")
+
+    def test_getAuthors(self):
+        self.assertEqual(self.cinta.getAuthors(), [self.radit, self.boyke])
+
+    def test_getPrice(self):
+        self.assertEqual(self.cinta.getPrice(), 100)
+
+    def test_setPrice(self):
+        self.cinta.setPrice(80)
+        self.assertEqual(self.cinta.getPrice(), 80)
+
+    def test_getQty(self):
+        self.assertEqual(self.cinta.getQty(), 10)
+
+    def test_setQty(self):
+        self.cinta.setQty(5)
+        self.assertEqual(self.cinta.getQty(), 5)
+
+    def test_toString(self):
+        self.assertEqual(self.cinta.toString(), 
+        "Book[name=Cinta Brontosaurus,"
+        "authors={Author[name=Radit,email=radit@radit.com,gender=m], Author[name=Boyke,email=boyke@boyke.com,gender=m]},"
+        "price=100,qty=10]")
+
+    def test_getAuthorNames(self):
+        self.assertEqual(self.cinta.getAuthorNames(), "Radit,Boyke")
+
+
+if __name__ == "__main__":
+    unittest.main()
